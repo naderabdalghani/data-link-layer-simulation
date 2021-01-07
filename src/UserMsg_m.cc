06f478b0002233ac62310ab95078588d65221e0b
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2021-01-07 21:49:30 +0200

    Fix UserMsg Includes

--- src/UserMsg_m.cc
+++ src/UserMsg_m.cc
@@ -1,5 +1,5 @@
 //
-// Generated file, do not edit! Created by nedtool 5.6 from userMsg.msg.
+// Generated file, do not edit! Created by nedtool 5.6 from UserMsg.msg.
 //
 
 // Disable warnings about unused variables, empty switch stmts, etc:
@@ -26,7 +26,7 @@
 
 #include <iostream>
 #include <sstream>
-#include "userMsg_m.h"
+#include "UserMsg_m.h"
 
 namespace omnetpp {
 
--- src/UserMsg_m.h
+++ src/UserMsg_m.h
@@ -1,3 +1,7 @@
+//
+// Generated file, do not edit! Created by nedtool 5.6 from UserMsg.msg.
+//
+
 #ifndef __USERMSG_M_H
 #define __USERMSG_M_H
 
@@ -13,6 +17,42 @@
 #endif
 
 
+
+/**
+ * Class generated from <tt>UserMsg.msg:1</tt> by nedtool.
+ * <pre>
+ * packet UserMsg
+ * {
+ *     \@customize(true);
+ *     int someField;
+ *     string msg;
+ * }
+ * </pre>
+ *
+ * UserMsg_Base is only useful if it gets subclassed, and UserMsg is derived from it.
+ * The minimum code to be written for UserMsg is the following:
+ *
+ * <pre>
+ * class UserMsg : public UserMsg_Base
+ * {
+ *   private:
+ *     void copy(const UserMsg& other) { ... }
+
+ *   public:
+ *     UserMsg(const char *name=nullptr, short kind=0) : UserMsg_Base(name,kind) {}
+ *     UserMsg(const UserMsg& other) : UserMsg_Base(other) {copy(other);}
+ *     UserMsg& operator=(const UserMsg& other) {if (this==&other) return *this; UserMsg_Base::operator=(other); copy(other); return *this;}
+ *     virtual UserMsg *dup() const override {return new UserMsg(*this);}
+ *     // ADD CODE HERE to redefine and implement pure virtual functions from UserMsg_Base
+ * };
+ * </pre>
+ *
+ * The following should go into a .cc (.cpp) file:
+ *
+ * <pre>
+ * Register_Class(UserMsg)
+ * </pre>
+ */
 class UserMsg_Base : public ::omnetpp::cPacket
 {
   protected:
