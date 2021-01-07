86cad1e3af67ec745ac30f43c3e7506f5c9fcf58
Muhanad <muhanad.atef23@gmail.com>
2021-01-07 18:29:11 +0200

    Update Node Class to Use UserMsg Class

--- src/Node.cc
+++ src/Node.cc
@@ -1,41 +1,76 @@
 #include "Node.h"
+#include "userMsg_m.h"
+#include <dirent.h>
+#include <string>
+using namespace std;
+
+const char* messages_directory = "./../src/messages";
 
 Define_Module(Node);
-
 void Node::initialize()
 {
-    if (strcmp(getName(), "hub") != 0) {
+    // Reading files in the "Msg files" directory and save them in files vector
+    DIR *dir;
+    struct dirent *diread;
+    vector<char *> files;
+    if ((dir = opendir(messages_directory)) != nullptr)
+    {
+        while ((diread = readdir(dir)) != nullptr)
+        {
+            int size = std::string(diread->d_name).size();
+            std::string s = diread->d_name;
+            if (s.size() > 3)
+                s = s.substr(size - 3);
+            //s = s[size - 3] + s[size - 2] + s[size - 1];
+            if (s.compare("txt") == 0)
+                files.push_back(diread->d_name);
+        }
+        closedir(dir);
+    }
+    for (auto file : files)
+        std::cout << file << "\n ";
+
+    if (strcmp(getName(), "hub") != 0)
+    {
         double interval = exponential(1 / par("lambda").doubleValue());
-        scheduleAt(simTime() + interval, new cMessage(""));
+        scheduleAt(simTime() + interval, new UserMsg_Base(""));
     }
 }
 
-void Node::handleMessage(cMessage *msg)
+void Node::handleMessage(cMessage *cmsg)
 {
-    if (strcmp(getName(), "hub") == 0) {
+    UserMsg_Base *msg = check_and_cast<UserMsg_Base *>(cmsg);
+    // Hub
+    if (strcmp(getName(), "hub") == 0)
+    {
         int dest, src;
         src = atoi(msg->getName());
-        do {
+        do
+        {
             dest = uniform(0, gateSize("outs"));
-        } while(dest == src);
+        } while (dest == src);
 
         std::stringstream ss;
         ss << dest;
-        EV << "Sending to node #"<< ss.str() <<" from " << getName() << "\n";
+        EV << "Sending to node #" << ss.str() << " from " << getName() << "\n";
         delete msg;
-        msg = new cMessage(ss.str().c_str());
+        msg = new UserMsg_Base(ss.str().c_str());
         send(msg, "outs", dest);
     }
-    else {
-        if (msg->isSelfMessage()) {
+    // For all nodes except hub
+    else
+    {
+        if (msg->isSelfMessage())
+        {
             delete msg;
-            msg = new cMessage(std::to_string(getIndex()).c_str());
+            msg = new UserMsg_Base(std::to_string(getIndex()).c_str());
             send(msg, "outs", 0);
             double interval = exponential(1 / par("lambda").doubleValue());
             EV << ". Scheduled a new packet after " << interval << "s";
-            scheduleAt(simTime() + interval, new cMessage(""));
+            scheduleAt(simTime() + interval, new UserMsg_Base(""));
         }
-        else {
+        else
+        {
             if (atoi(msg->getName()) == getIndex())
                 bubble("Message received");
             else
@@ -44,7 +79,3 @@ void Node::handleMessage(cMessage *msg)
         }
     }
 }
-
-
-
-
--- src/Node.h
+++ src/Node.h
@@ -1,16 +1,15 @@
-#ifndef __NODE_H_
-#define __NODE_H_
+#ifndef __STAR_NODE_H_
+#define __STAR_NODE_H_
 
 #include <omnetpp.h>
-using namespace omnetpp;
 
+using namespace omnetpp;
 
 class Node : public cSimpleModule
 {
-  protected:
-    virtual void initialize();
-    virtual void handleMessage(cMessage *msg);
+protected:
+  virtual void initialize();
+  virtual void handleMessage(cMessage *msg);
 };
-
 
 #endif
