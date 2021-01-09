c424e223227e39211c5296a9b0e05868e7d2a32c
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2021-01-09 17:42:13 +0200

    Make Channels Randomly Discard and Delay Frames

--- src/NoisyChannel.cc
+++ src/NoisyChannel.cc
@@ -2,18 +2,85 @@
 
 Define_Channel(NoisyChannel);
 
-NoisyChannel::NoisyChannel(const char *name) : cChannel(name) {}
+simsignal_t NoisyChannel::channelBusySignal;
+simsignal_t NoisyChannel::messageSentSignal;
+simsignal_t NoisyChannel::messageDiscardedSignal;
 
-void NoisyChannel::processMessage(cMessage *msg, simtime_t t, result_t& result) {}
+NoisyChannel::NoisyChannel(const char *name) : cChannel(name) {
+    txFinishTime = -1;
+}
 
-double NoisyChannel::getNominalDatarate() const {return 0;}
+void NoisyChannel::initialize()
+{
+    channelBusySignal = registerSignal("channelBusy");
+    messageSentSignal = registerSignal("messageSent");
+    messageDiscardedSignal = registerSignal("messageDiscarded");
+    emit(channelBusySignal, 0);
+}
 
-bool NoisyChannel::isTransmissionChannel() const {return false;}
+void NoisyChannel::rereadPars()
+{
+    delayMin = par("delayMin").doubleValue();
+    delayMax = par("delayMax").doubleValue();
+    delayProbability = par("delayProbability").doubleValue();
+    discardingProbability = par("discardingProbability").doubleValue();
+    if (delayMin < 0 ||
+        delayMax < 0 ||
+        delayMin > delayMax ||
+        delayProbability < 0 ||
+        delayProbability > 100 ||
+        discardingProbability < 0 ||
+        discardingProbability > 100
+        )
+        throw cRuntimeError(this, "Invalid channel parameter(s)");
+}
 
-simtime_t NoisyChannel::calculateDuration(cMessage *msg) const {return SIMTIME_ZERO;}
+void NoisyChannel::handleParameterChange(const char *)
+{
+    rereadPars();
+}
 
-simtime_t NoisyChannel::getTransmissionFinishTime() const {return SIMTIME_ZERO;}
+void NoisyChannel::finish()
+{
+    if (txFinishTime != -1 && mayHaveListeners(channelBusySignal)) {
+        cTimestampedValue tmp(txFinishTime, 0L);
+        emit(channelBusySignal, &tmp);
+    }
+}
 
-bool NoisyChannel::isBusy() const {return false;}
+void NoisyChannel::processMessage(cMessage *msg, simtime_t t, result_t& result) {
+    double rand = uniform(0, 100);
+    if (rand <= discardingProbability) {
+        result.discard = true;
+        cTimestampedValue tmp(t, msg);
+        emit(messageDiscardedSignal, &tmp);
+        EV << "Message discarded" << endl;
+        return;
+    }
 
-void NoisyChannel::forceTransmissionFinishTime(simtime_t t) {}
+    if (txFinishTime != -1 && mayHaveListeners(channelBusySignal)) {
+        cTimestampedValue tmp(txFinishTime, 0L);
+        emit(channelBusySignal, &tmp);
+    }
+
+    rand = uniform(0, 100);
+    if (rand <= delayProbability) {
+        result.delay = uniform(delayMin, delayMax);
+        txFinishTime = t + result.delay;
+        EV << "Message delayed with value = " << result.delay << endl;
+    }
+    if (mayHaveListeners(messageSentSignal)) {
+        MessageSentSignalValue tmp(t, msg, &result);
+        emit(messageSentSignal, &tmp);
+    }
+}
+
+bool NoisyChannel::isTransmissionChannel() const { return true; }
+
+simtime_t NoisyChannel::getTransmissionFinishTime() const { return txFinishTime; }
+
+void NoisyChannel::forceTransmissionFinishTime(simtime_t t) { txFinishTime = t; }
+
+double NoisyChannel::getNominalDatarate() const { return 0; }
+
+simtime_t NoisyChannel::calculateDuration(cMessage *msg) const { return SIMTIME_ZERO; }
--- src/NoisyChannel.h
+++ src/NoisyChannel.h
@@ -7,16 +7,32 @@ using namespace omnetpp;
 
 class NoisyChannel : public cChannel
 {
+private:
+    simtime_t delayMin;
+    simtime_t delayMax;
+    simtime_t txFinishTime;
+    double delayProbability;
+    double discardingProbability;
+
+    void checkState() const  {
+        if (!parametersFinalized()) throw cRuntimeError(this, E_PARAMSNOTREADY);
+    }
+protected:
+    static simsignal_t channelBusySignal;
+    static simsignal_t messageSentSignal;
+    static simsignal_t messageDiscardedSignal;
+    void rereadPars();
+    virtual void handleParameterChange(const char *parname) override;
+    virtual void finish() override;
 public:
     explicit NoisyChannel(const char *name=nullptr);
-    static NoisyChannel *create(const char *name);
+    virtual void initialize() override;
     virtual void processMessage(cMessage *msg, simtime_t t, result_t& result) override;
-    virtual double getNominalDatarate() const override;
     virtual bool isTransmissionChannel() const override;
-    virtual simtime_t calculateDuration(cMessage *msg) const override;
     virtual simtime_t getTransmissionFinishTime() const override;
-    virtual bool isBusy() const override;
     virtual void forceTransmissionFinishTime(simtime_t t) override;
+    virtual double getNominalDatarate() const override;
+    virtual simtime_t calculateDuration(cMessage *msg) const override;
 };
 
 #endif
